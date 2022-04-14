bb2285d7e42608dc84f4f6117f41b5cf7dbfb5fc
Simba830 <JamesRyan01031@outlook.com>
2022-04-14 11:15:51 +0900

    modified for configuration

--- src/config/index.tsx
+++ src/config/index.tsx
@@ -1,2 +1,5 @@
-export const BASE_SERVER_API_URL = "http://localhost:8080/";
-export const SERVER_API_URL = "http://localhost:8080/api/v1";
+// export const BASE_SERVER_API_URL = `http://localhost:8080/`;
+// export const SERVER_API_URL = `http://localhost:8080/api/v1`;
+
+export const BASE_SERVER_API_URL = `https://test-blog-for-beaumont.herokuapp.com`;
+export const SERVER_API_URL = `${BASE_SERVER_API_URL}/api/v1`;
